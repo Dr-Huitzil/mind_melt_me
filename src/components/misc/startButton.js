f8bcf5a13b27e00702690c374760b31ebe6fc83b
Dr-Huitzil <gotenkscoolness300@gmail.com>
2024-04-15 18:07:44 -0700

    fixed up the start button and added the logo

--- src/components/misc/startButton.js
+++ src/components/misc/startButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './styles/startButton.css';
+import logoSrc from './imgs/brain-logo.png';
 
 const StartButton = ({ toggleStartMenu }) => {
     return (
         <button className="start-button" onClick={toggleStartMenu}>
+            <img src={logoSrc} alt="logo" className="start-button-logo" />
             Start
         </button>
     );
