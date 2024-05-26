880046199b102a44451b608fc463c14cb407bfef
Dr-Huitzil <gotenkscoolness300@gmail.com>
2024-05-26 16:08:23 -0700

    i added placeholder images that would allow me to fill in and see how the desktop icons work

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Preloader from './components/Prelaoder';
 import Desktop from './components/Desktop';
 import StartMenu from './components/StartMenu';
 import Taskbar from './components/Taskbar';
+import Window from './components/Window';
 import './styles/global.css';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [startMenuVisible, setStartMenuVisible] = useState(false);
   const [runningApps, setRunningApps] = useState([]);
+  const [openWindows, setOpenWindows] = useState([]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -34,6 +36,10 @@ const App = () => {
     }
   };
 
+  const handleCloseWindow = (id) => {
+    setOpenWindows(openWindows.filter(window => window.id !== id));
+  }
+
   return (
     <div>
       {loading && <Preloader />}
@@ -47,6 +53,16 @@ const App = () => {
           <button className="start-button" onClick={toggleStartMenu}>
             Start
           </button>
+          {openWindows.map(window => (
+            <Window
+              key={window.id}
+              id={window.id}
+              title={window.name}
+              onClose={() => handleCloseWindow(window.id)}
+            >
+              <div> This is the {window.title} window</div>
+            </Window>
+          ))}
         </div>
       )}
     </div>
--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import '../styles/Desktop.css';
-import meIcon from "../assets/desktop-icons/me-icon.png";
-
-const icons = [
-    { id: 1, src: meIcon, name: 'My Icon', action: () => alert('My Icon Clicked') },
-];
 
 const Desktop = ({ onIconClick }) => {
+    const icons = [
+        { id: 'about', name: 'About', icon: require('../assets/desktop-icons/me-icon.png') },
+        { id: 'chat', name: "Chatbox", icon: require('../assets/desktop-icons/kirby1.jpg') },
+        { id: 'calculator', name: 'Calculator', icon: require('../assets/desktop-icons/kirby2.jpg') },
+        { id: 'shop', name: 'Shop', icon: require('../assets/desktop-icons/kirby3.png') },
+        { id: 'contact', name: 'Contact', icon: require('../assets/desktop-icons/kirby4.jpg') },
+        { id: 'music', name: 'Music', icon: require('../assets/desktop-icons/kirby5.jpg') },
+        { id: 'video', name: 'Video', icon: require('../assets/desktop-icons/kirby6.png') },
+        { id: 'games', name: 'Games', icon: require('../assets/desktop-icons/kirby1.jpg') },
+        { id: 'images', name: 'Images', icon: require('../assets/desktop-icons/kirby2.jpg') },
+        { id: 'the lore', name: 'The Lore', icon: require('../assets/desktop-icons/kirby3.png') },
+        { id: 'notepad', name: 'Notepad', icon: require('../assets/desktop-icons/kirby4.jpg') },
+        { id: 'faq', name: 'FAQ', icon: require('../assets/desktop-icons/kirby5.jpg') },
+        { id: 'moreLinks', name: 'More Links', icon: require('../assets/desktop-icons/kirby1.jpg') },
+        { id: 'Blog', name: 'Blog', icon: require('../assets/desktop-icons/kirby6.png') }
+    ];
+
     return (
-        <div className='desktop-bg'>
-            <div className='desk-icons-wrapper'>
-                {icons.map(icon => (
-                    <div key={icon.id} className='desktop-icon' onClick={() => onIconClick(icon)}>
-                        <img src={icon.src} alt={icon.name} />
-                        <span className='icon-text'>{icon.name}</span>
-                    </div>
-                ))}
-            </div>
+        <div className="desktop">
+            {icons.map(icon => (
+                <div
+                    key={icon.id}
+                    className="desktop-icon"
+                    onClick={() => onIconClick(icon.id)}
+                >
+                    <img src={icon.icon} alt={icon.name} />
+                    <span>{icon.name}</span>
+                </div>
+            ))}
         </div>
     );
 };
