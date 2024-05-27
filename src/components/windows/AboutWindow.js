ea504f81baad068ab35902fd63354b7139e7b755
Dr-Huitzil <gotenkscoolness300@gmail.com>
2024-05-26 17:19:36 -0700

    the about window finally shows up somewhere in the middle now

--- src/components/windows/AboutWindow.js
+++ src/components/windows/AboutWindow.js
@@ -20,35 +20,37 @@ const AboutWindow = ({ onClose }) => {
     }, []);
 
     return (
-        <Draggable handle=".window-title-bar" defaultPosition={windowPosition}>
-            <ResizableBox
-                width={400}
-                height={300}
-                minConstraints={[300, 200]}
-                maxConstraints={[600, 400]}
-                className="resizable-box"
-            >
-                <div className="window" ref={windowRef}>
-                    <div className="window-title-bar">
-                        <span className="window-title">About</span>
-                        <button className="window-close-button" onClick={onClose}>X</button>
+        <Draggable handle=".window-title-bar">
+            <div style={{ position: 'absolute', left: windowPosition.x, top: windowPosition.y }}>
+                <ResizableBox
+                    width={400}
+                    height={300}
+                    minConstraints={[300, 200]}
+                    maxConstraints={[600, 400]}
+                    className="resizable-box"
+                >
+                    <div className="window" ref={windowRef}>
+                        <div className="window-title-bar">
+                            <span className="window-title">About</span>
+                            <button className="window-close-button" onClick={onClose}>X</button>
+                        </div>
+                        <div className="window-content">
+                            <h2>Mind Melt</h2>
+                            <p>
+                                What exactly is Mind Melt? You may have heard of us or seen our stickers scattered
+                                around campus but do you ever question what Mind Melt is supposed to be? Mind Melt is
+                                the melting of ideas, a creative outlet, that encourages people and students to express
+                                themselves creatively. It all started with a simple radio show on [Otter Media] hosted
+                                on the CSUMB channel, The Current back in the fall of 2022. It has slowly grown into a
+                                much greater community that has a passion for music, art, and creativity. We are a group
+                                of students that are passionate about music and art and we want to share that passion with
+                                whoever finds us. It all began with music but we are slowly expanding our mind-melting ideas
+                                into the realms of fashion, photography, and many other creative fields.
+                            </p>
+                        </div>
                     </div>
-                    <div className="window-content">
-                        <h2>Mind Melt</h2>
-                        <p>
-                            What exactly is Mind Melt? You may have heard of us or seen our stickers scattered
-                            around campus but do you ever question what Mind Melt is supposed to be? Mind Melt is
-                            the melting of ideas, a creative outlet, that encourages people and students to express
-                            themselves creatively. It all started with a simple radio show on [Otter Media] hosted
-                            on the CSUMB channel, The Current back in the fall of 2022. It has slowly grown into a
-                            much greater community that has a passion for music, art, and creativity. We are a group
-                            of students that are passionate about music and art and we want to share that passion with
-                            whoever finds us. It all began with music but we are slowly expanding our mind-melting ideas
-                            into the realms of fashion, photography, and many other creative fields.
-                        </p>
-                    </div>
-                </div>
-            </ResizableBox>
+                </ResizableBox>
+            </div>
         </Draggable>
     );
 };
