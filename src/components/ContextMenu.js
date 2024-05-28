c1e7c1e53ba71170632256583fc24d7d8d9e0fa9
Dr-Huitzil <gotenkscoolness300@gmail.com>
2024-05-27 17:01:24 -0700

    making small changes to the desktop

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,18 +1,29 @@
+// ContextMenu.js
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styles/ContextMenu.css';
 
 const ContextMenu = ({ position, visible, onClose, onNewShortcut }) => {
-    if (!visible) return null;
+    if (!visible) {
+        return null;
+    }
 
     return (
-        <div className='context-menu' style={{ top: position.y, left: position.x }} onClick={onClose}>
-            <ul>
-                <li onClick={onNewShortcut}>New Shortcut</li>
-                <li onClick={onClose}>Change Background</li>
-                <li onClick={onClose}>Refresh</li>
-            </ul>
+        <div className="context-menu" style={{ top: position.y, left: position.x }}>
+            <div className="context-menu-item" onClick={onNewShortcut}>New Shortcut</div>
+            <div className="context-menu-item" onClick={onClose}>Close</div>
         </div>
     );
+};
+
+ContextMenu.propTypes = {
+    position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+    }).isRequired,
+    visible: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    onNewShortcut: PropTypes.func.isRequired,
 };
 
 export default ContextMenu;
