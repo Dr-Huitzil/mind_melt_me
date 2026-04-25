af8e64519ce994f747ee770b11981b8247619ca3
Dr-Huitzil <ialierreyes@gmail.com>
2026-04-25 16:33:16 -0700

    added posthog to the project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 import { WindowProvider } from './contexts/WindowContext';
+import posthog from 'posthog-js';
+
+posthog.init(process.env.REACT_APP_PUBLIC_POSTHOG_KEY, {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  person_profiles: 'identified_only',
+});
 
 // Create a root.
 const root = createRoot(document.getElementById('root'));
